748c97551fc2a66af91fd2698249e1f0e01841a2
tangmengcheng <1018663766@qq.com>
2020-02-02 02:09:22 +0800

    feat(examples and  src folders): 编写基本xhr, 运行demo
    编写基础请求代码, 创建入口文件, 定义 AxiosRequestConfig 接口类型, 利用 XMLHttpRequest 发送请求, demo 编写

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'post'
+  | 'POST'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
+  | 'delete'
+  | 'DELETE'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
