fb998f8fed0620f7d6389989c207dc37552896c2
tangmengcheng <1018663766@qq.com>
2020-02-10 01:00:49 +0800

    feat(examples and error files): 错误信息增强
    createError 方法应用+ 导出类型定义

--- src/types/index.ts
+++ src/types/index.ts
@@ -33,3 +33,11 @@ export interface AxiosResponse {
   request: any
 }
 export interface AxiosPromise extends Promise<AxiosResponse> {}
+
+export interface AxiosError extends Error {
+  isAxiosError: boolean
+  config: AxiosRequestConfig
+  code?: string | null
+  request?: any
+  response?: AxiosResponse
+}
